6aeecc5637d933160f363ee55c1a4c7b52dc288b
harikrishnanvr <djharry123@gmail.com>
2017-11-15 10:52:03 +0530

    Added Migration MembershipType and newsletter subscription

--- Vidly/Models/Customer.cs
+++ Vidly/Models/Customer.cs
@@ -9,7 +9,9 @@ namespace Vidly.Models
     {
         public int Id { get; set; }
         public string Name { get; set; }
-
+        public bool IsSubscribedToNewsletter { get; set; }
+        public MembershipType MembershipType { get; set; }
+        public byte MembershipTypeId { get; set; }
         public IEnumerable<Customer> Customers { get; set; }
     }
 }
