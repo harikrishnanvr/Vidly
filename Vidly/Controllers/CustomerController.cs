615f6d4d36c0b3d9033ffd1adefde922e48aa151
harikrishnanvr <djharry123@gmail.com>
2017-11-16 11:57:59 +0530

    Populated data from database. removed hardcoded data. Modified model by removing the ienumerable from it. added @model IEnumerable<Vidly.Models.Customer> in view

--- Vidly/Controllers/CustomerController.cs
+++ Vidly/Controllers/CustomerController.cs
@@ -9,43 +9,72 @@ namespace Vidly.Controllers
 {
     public class CustomerController : Controller
     {
+        private ApplicationDbContext _context;
+
+        public CustomerController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
 
         // GET: Customer
         public ActionResult Index()
         {
-            var customer = new Customer();
+            //var customer = new Customer();
+            //customer.Customers = Getcustomer();
+            //return View(customer);
 
-            customer.Customers = Getcustomer();
+            var customer = _context.Customers.ToList();
             return View(customer);
         }
 
-        private IEnumerable<Customer> Getcustomer()
-        {
-            return new List<Customer>
-            {
-                new Customer {Name="Hari",Id=1 },
-                new Customer {Name="Anna",Id=2 }
+        //private IEnumerable<Customer> Getcustomer()
+        //{
+        //    return new List<Customer>
+        //    {
+        //        new Customer {Name="Hari",Id=1 },
+        //        new Customer {Name="Anna",Id=2 }
 
-            };
+        //    };
 
-        }
+        //}
 
         [Route("customer/details/{id}")]
         public ActionResult Details(int? id)
         {
-            
-            var customer = Getcustomer().SingleOrDefault(row => row.Id == id);
-            if (customer!=null && customer.Customers.Count() == 0)
-            {
-                ViewBag.customerName = "We don't have any customers yet";
-              
-            }
-            else
-            {
-                ViewBag.customerName = customer.Name;
-            }
+            var customer = _context.Customers.SingleOrDefault(row => row.Id == id);
+            if (customer == null)
+                return HttpNotFound();
 
-            return View(ViewBag);
+            return View(customer);
+            //if (customer != null && customer.Count() == 0)
+            //{
+            //    ViewBag.customerName = "We don't have any customers yet";
+
+            //}
+            //else
+            //{
+            //    ViewBag.customerName = customer.Name;
+            //}
+
+            //return View(ViewBag);
+
+            //var customer = Getcustomer().SingleOrDefault(row => row.Id == id);
+            //if (customer!=null && customer.Customers.Count() == 0)
+            //{
+            //    ViewBag.customerName = "We don't have any customers yet";
+
+            //}
+            //else
+            //{
+            //    ViewBag.customerName = customer.Name;
+            //}
+
+            //return View(ViewBag);
 
         }
     }
