3960904ddb4cf739937b805eaeb132437a75a71d
harikrishnanvr <djharry123@gmail.com>
2017-11-25 16:05:44 +0530

    037-Custom Labels for controls

--- Vidly/Models/Customer.cs
+++ Vidly/Models/Customer.cs
@@ -13,6 +13,8 @@ namespace Vidly.Models
         [StringLength(255)]
         public string Name { get; set; }
         public bool IsSubscribedToNewsletter { get; set; }
+
+        [Display(Name="Date of Birth")]
         public DateTime? Birthdate { get; set; }
         public MembershipType MembershipType { get; set; }
         public byte MembershipTypeId { get; set; }
