16c96be52c81bae3498dcc5d51fc790c791da3ac
harikrishnanvr <djharry123@gmail.com>
2017-11-29 12:38:26 +0530

    050-Custom Validation, Added business rule validation as custom data anotation 'Min18YearsIfAMember'

--- Vidly/Models/Customer.cs
+++ Vidly/Models/Customer.cs
@@ -10,13 +10,14 @@ namespace Vidly.Models
     {
         public int Id { get; set; }
 
-        [Required]
+        [Required(ErrorMessage ="Please enter Customer's Name")]
         [StringLength(255)]
         public string Name { get; set; }
 
         public bool IsSubscribedToNewsletter { get; set; }
 
         [Display(Name="Date of Birth")]
+        [Min18YearsIfAMember]
         public DateTime? Birthdate { get; set; }
        
         public MembershipType MembershipType { get; set; }
---
+++ Vidly/Models/Min18YearsIfAMember.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+namespace Vidly.Models
+{
+    public class Min18YearsIfAMember : ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            var Customer = (Customer)validationContext.ObjectInstance;
+
+            if (Customer.MembershipTypeId==0 ||Customer.MembershipTypeId == 1)
+                return ValidationResult.Success;
+
+            if (Customer.Birthdate == null)
+                return new ValidationResult("Birthday is required");
+
+            var age = System.DateTime.Now.Year - Customer.Birthdate.Value.Year;
+
+            return (age > 18) ?
+                     ValidationResult.Success
+                : new ValidationResult("Customer must be above 18 years to go over this membership"); 
+        }
+    }
+}
